524d72ba5d938663517d878379ee9670f705d8c8
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-10-28 00:28:13 -0300

    Adiciona rotas completas para campanhas e filas de campanhas (admin e estabelecimento)

--- src/Frontend/cannoli-intelligence/src/App.jsx
+++ src/Frontend/cannoli-intelligence/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import LoginPage from "./pages/LoginPage/LoginPage";
 import { Routes, Route, Navigate } from "react-router-dom";
 import CampaignsEstab from "./pages/CampaignsEstab/CampaignsEstab";
-import './App.css'
-import "./styles.css";
-
+import CampaignsAdmin from './pages/CampaignsAdmin/CampaignsAdmin';
+import CampaignQueueEstab from './pages/CampaignQueueEstab/CampaignQueueEstab';
+import CampaignQueueAdmin from './pages/CampaignQueueAdmin/CampaignQueueAdmin';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +14,10 @@ function App() {
       <div>
             <Routes>
                 <Route path="/" element={<LoginPage />} />
-                <Route path="/campanhas" element={<CampaignsEstab />} />
+                <Route path="/campanhas/estab" element={<CampaignsEstab />} />
+                <Route path="/campanhas/admin" element={<CampaignsAdmin />} />
+                <Route path="/queue/estab" element={<CampaignQueueEstab />} />
+                <Route path="/queue/admin" element={<CampaignQueueAdmin />} />
             </Routes>
         </div>
     </>
