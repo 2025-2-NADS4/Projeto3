31d5388afa6bb4fb7e3843692223787d535a3390
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-11-02 00:55:53 -0300

    Atualiza servidor para usar rotas centralizadas da API

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import db from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import campaignRoutes from './routes/campaignRoutes.js';
+import apiRoutes from './routes/apiRoutes.js';
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/auth', userRoutes);
-app.use('/api', campaignRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
