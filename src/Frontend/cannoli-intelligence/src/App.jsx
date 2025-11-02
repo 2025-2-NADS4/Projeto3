7659402e82e612ffcd0f142c0ff4aa035176af23
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-11-02 00:49:31 -0300

    Adiciona rotas de clientes (admin e estabelecimento) no frontend

--- src/Frontend/cannoli-intelligence/src/App.jsx
+++ src/Frontend/cannoli-intelligence/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import LoginPage from "./pages/LoginPage/LoginPage";
 import { Routes, Route, Navigate } from "react-router-dom";
-import CampaignsEstab from "./pages/CampaignsEstab/CampaignsEstab";
-import CampaignsAdmin from './pages/CampaignsAdmin/CampaignsAdmin';
-import CampaignQueueEstab from './pages/CampaignQueueEstab/CampaignQueueEstab';
-import CampaignQueueAdmin from './pages/CampaignQueueAdmin/CampaignQueueAdmin';
+import CampaignsEstab from "./pages/Campaigns/CampaignsEstab";
+import CampaignsAdmin from './pages/Campaigns/CampaignsAdmin';
+import CampaignQueueEstab from './pages/CampaignQueue/CampaignQueueEstab';
+import CampaignQueueAdmin from './pages/CampaignQueue/CampaignQueueAdmin';
+import CustomersEstab from './pages/Customers/CustomersEstab';
+import CustomersAdmin from './pages/Customers/CustomersAdmin';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +20,8 @@ function App() {
                 <Route path="/campanhas/admin" element={<CampaignsAdmin />} />
                 <Route path="/queue/estab" element={<CampaignQueueEstab />} />
                 <Route path="/queue/admin" element={<CampaignQueueAdmin />} />
+                <Route path="/clientes/estab" element={<CustomersEstab />} />
+                <Route path="/clientes/admin" element={<CustomersAdmin />} />
             </Routes>
         </div>
     </>
