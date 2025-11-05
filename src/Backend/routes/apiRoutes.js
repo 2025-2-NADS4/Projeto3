72384666b1bf1d4a4ebd50f66a9c5dba9483bb30
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-11-05 19:56:31 -0300

    Integra rotas de visão geral do admin ao roteador principal da API

--- src/Backend/routes/apiRoutes.js
+++ src/Backend/routes/apiRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 import campaignRoutes from './campaignRoutes.js';
 import clientRoutes from './clientRoutes.js';
 import orderRoutes from './orderRoutes.js';
+import adminRoutes from './adminRoutes.js';
 
 const router = express.Router();
 
 router.use(campaignRoutes);
 router.use(clientRoutes);
 router.use(orderRoutes);
+router.use(adminRoutes);
 
 export default router;
