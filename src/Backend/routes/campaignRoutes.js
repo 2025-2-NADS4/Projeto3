bd61d5c554aba6856c828733502439350d1e71aa
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-11-08 12:17:10 -0300

    Inclusão de rotas de sugestões de campanhas por perfil

--- src/Backend/routes/campaignRoutes.js
+++ src/Backend/routes/campaignRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import autenticarToken from '../middlewares/autenticarToken.js';
-import { getCampanhasEstabelecimento, getCampanhasAdmin } from '../controllers/campaignController.js';
+import { getCampanhasEstabelecimento, getCampanhasAdmin, getCampanhasSugestoesAdmin, getCampanhasSugestoesEstabelecimento } from '../controllers/campaignController.js';
 import { getCampaignQueueAdmin, getCampaignQueueEstabelecimento } from '../controllers/campaignQueueController.js';
 
 const router = Router();
@@ -9,5 +9,7 @@ router.get('/estabelecimento/campanhas', autenticarToken, getCampanhasEstabeleci
 router.get('/admin/campanhas', autenticarToken, getCampanhasAdmin);
 router.get('/estabelecimento/campaignqueue', autenticarToken, getCampaignQueueEstabelecimento);
 router.get('/admin/campaignqueue', autenticarToken, getCampaignQueueAdmin);
+router.get('/estabelecimento/campanhas/sugestoes', autenticarToken, getCampanhasSugestoesEstabelecimento);
+router.get('/admin/campanhas/sugestoes', autenticarToken, getCampanhasSugestoesAdmin);
 
 export default router;
