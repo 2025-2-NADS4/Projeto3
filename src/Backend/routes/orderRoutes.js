fc9376eb794dbe580f98eeefaf8e1b431749dee1
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-11-09 19:57:55 -0300

    Adicionar rota de exportação PDF para pedidos

--- src/Backend/routes/orderRoutes.js
+++ src/Backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import autenticarToken from '../middlewares/autenticarToken.js';
-import { getPedidosEstabelecimento, getPedidosAdmin } from '../controllers/orderController.js';
+import { getPedidosEstabelecimento, getPedidosAdmin, exportPedidosEstabPdf } from '../controllers/orderController.js';
 
 const router = Router();
 
 router.get("/estabelecimento/pedidos", autenticarToken, getPedidosEstabelecimento);
 router.get("/admin/pedidos", autenticarToken, getPedidosAdmin);
+router.get("/estabelecimento/pedidos/export/pdf", autenticarToken, exportPedidosEstabPdf);
 
 export default router;
