ce0f858ab675dd0dfa86156a24c792b9f433d67d
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-11-09 19:57:45 -0300

    Adicionar rota de exportação PDF para clientes

--- src/Backend/routes/clientRoutes.js
+++ src/Backend/routes/clientRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import autenticarToken from '../middlewares/autenticarToken.js';
-import { getClientesEstabelecimento, getClientesAdmin } from '../controllers/clientController.js';
+import { getClientesEstabelecimento, getClientesAdmin, exportClientesEstabPdf } from '../controllers/clientController.js';
 
 const router = Router();
 
 router.get('/estabelecimento/clientes', autenticarToken, getClientesEstabelecimento);
 router.get('/admin/clientes', autenticarToken, getClientesAdmin);
+router.get('/estabelecimento/clientes/export/pdf', autenticarToken, exportClientesEstabPdf);
 
 export default router;
