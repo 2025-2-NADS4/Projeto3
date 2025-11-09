f47aeec220166e7aa7cc391f1583ca53c15480c6
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-11-09 19:44:52 -0300

    Adicionar rota /clientes-risco/export/pdf no módulo de risco

--- src/Backend/routes/clientRiskRoutes.js
+++ src/Backend/routes/clientRiskRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import autenticarToken from '../middlewares/autenticarToken.js';
-import { getClientesRiscoEstabelecimento, getClientesRiscoAdmin } from '../controllers/clientRiskController.js';
+import { getClientesRiscoEstabelecimento, getClientesRiscoAdmin, exportClientesRiscoEstabPdf } from '../controllers/clientRiskController.js';
 
 const router = Router();
 
 router.get("/estabelecimento/clientes-risco", autenticarToken, getClientesRiscoEstabelecimento);
 router.get("/admin/clientes-risco", autenticarToken, getClientesRiscoAdmin);
+router.get("/estabelecimento/clientes-risco/export/pdf", autenticarToken, exportClientesRiscoEstabPdf);
 
 export default router;
