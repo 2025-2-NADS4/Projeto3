b149457d2b7a916a117e4c011c85a5c60beb0b5d
Hebert Esteves <hebertesteves14.sp@gmail.com>
2025-11-10 20:03:16 -0300

    Cria rota de exportação PDF para pedidos

--- src/Backend/routes/orderRoutes.js
+++ src/Backend/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import autenticarToken from '../middlewares/autenticarToken.js';
-import { getPedidosEstabelecimento, getPedidosAdmin, exportPedidosEstabPdf } from '../controllers/orderController.js';
+import { getPedidosEstabelecimento, getPedidosAdmin, exportPedidosEstabPdf, exportPedidosAdminPdf } from '../controllers/orderController.js';
 
 const router = Router();
 
 router.get("/estabelecimento/pedidos", autenticarToken, getPedidosEstabelecimento);
 router.get("/admin/pedidos", autenticarToken, getPedidosAdmin);
 router.get("/estabelecimento/pedidos/export/pdf", autenticarToken, exportPedidosEstabPdf);
+router.get("/admin/pedidos/export/pdf", autenticarToken, exportPedidosAdminPdf);
 
 export default router;
